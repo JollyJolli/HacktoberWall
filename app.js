ea5f9a331b999f211a2305071ed519827f7972c2
Hiroto Tsujii <hitoto_t@kernel-map.co.jp>
2024-09-25 17:45:47 +0900

    Add my name to contributors.json And Addition of dark mode switching function

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
+// consts
+const MOON = "🌜";
+const SUN = "🌞";
+
 // Function to load participants from the JSON file
 async function loadContributors() {
-    const response = await fetch('contributors.json');
+    const response = await fetch("contributors.json");
     const contributors = await response.json();
     return contributors;
 }
 
 // Function to display participants on the mural
 async function displayContributors() {
-    const wall = document.getElementById('wall');
+    const wall = document.getElementById("wall");
     const contributors = await loadContributors();
-    wall.innerHTML = ''; // Clear the wall before adding new participants
+    wall.innerHTML = ""; // Clear the wall before adding new participants
 
-    contributors.forEach(contributor => {
-        const div = document.createElement('div');
-        div.classList.add('participant');
+    contributors.forEach((contributor) => {
+        const div = document.createElement("div");
+        div.classList.add("participant");
         div.textContent = contributor.name;
         wall.appendChild(div);
     });
 }
 
-// Initialize the wall with existing contributors
-window.onload = function() {
-    displayContributors();
+// Function to Switch light/dark mode
+function InitializeDarkMode() {
+    const toggleButton = document.getElementById("toggleButton");
+    const body = document.body;
+    const elements = document.querySelectorAll('div, footer');
+
+    // get from localstorage
+    const isDarkMode = localStorage.getItem("dark-mode");
+
+    if (isDarkMode === "enabled") {
+        body.classList.add("dark-mode");
+        elements.forEach(element => element.classList.add('dark-mode'));
+        toggleButton.textContent = MOON;
+    } else {
+        toggleButton.textContent = SUN;
+    }
+
+    // set click event 
+    toggleButton.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+        elements.forEach(element => element.classList.toggle('dark-mode'));
+        
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("dark-mode", "enabled");
+            toggleButton.textContent = MOON; 
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+            toggleButton.textContent = SUN; 
+        }
+    });
 }
+
+// Initialize the wall with existing contributors
+window.onload = function () {
+    displayContributors();
+    InitializeDarkMode();
+};
