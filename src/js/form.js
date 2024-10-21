978f9d327b02a1b8cde35f34cd5b5bcf3d9989aa
Mael Donnart <maeldonnart@hotmail.fr>
2024-10-21 22:37:46 +0200

    - fix some typo problem in hrml and css for the contact form

--- src/js/form.js
+++ src/js/form.js
@@ -3,41 +3,39 @@ const MOON = '<i class="fas fa-moon"></i>'
 const SUN = '<i class="fas fa-sun"></i>'
 
 document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+  .getElementById('contactForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault() // Prevent the default form submission
 
     // Display thank you message
     if (document.body.classList.contains('halloween-mode')) {
-      document.getElementById("thankYouMessage").textContent = "Thanks for your spooky feedback!";
+      document.getElementById('thankYouMessage').textContent =
+        'Thanks for your spooky feedback!'
     }
 
     // Add an animated effect to the thank you message for Halloween
     if (document.body.classList.contains('halloween-mode')) {
-      document.getElementById("thankYouMessage").style.textShadow = "2px 2px 10px orange, 2px 2px 20px red";
-      document.getElementById("thankYouMessage").style.display = "block";
+      document.getElementById('thankYouMessage').style.textShadow =
+        '2px 2px 10px orange, 2px 2px 20px red'
+      document.getElementById('thankYouMessage').style.display = 'block'
     }
 
     // Optionally, you can handle the form data here, such as sending it to a server
-    const comments = document.getElementById("comments").value;
-    const contact = document.getElementById("form-email").value;
+    const comments = document.getElementById('comments').value
+    const contact = document.getElementById('form-email').value
 
-    console.log("Feedback submitted:", { comments, contact });
+    console.log('Feedback submitted:', { comments, contact })
 
     // Clear the form fields
-    event.target.reset();
+    event.target.reset()
 
     // Delay for 2 seconds to let the user see the thank you message
     setTimeout(function () {
       // Redirect to index.html after 2 seconds
-      window.location.href = "index.html";
-    }, 2000);
+      window.location.href = 'index.html'
+    }, 2000)
     // Modify the thank you message for Halloween
-  }
-
-  );
-
-  });
+  })
 
 function initializeDarkMode() {
   const toggleButton = document.getElementById('toggleButton')
